61a6b676f06d6000baf86e049dc31f1a34f19cf5
rifandani <ipandani2505@gmail.com>
2023-09-27 15:17:49 +0700

    feat: adjust env schema, adjust root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,32 @@
-import { cookieApi } from '#shared/api/cookie.api';
-import { GlobalNav } from '#shared/components/templates/global-nav';
-import { LoadingSingleSkeleton } from '#shared/components/templates/loading-single-skeleton/index.client';
-import { userApi } from '#user/api/user.api';
+import { envSchema } from '#shared/api/env.schema';
 
 export default async function RootPage() {
-  const appAuthCookie = cookieApi.getAppAuthCookie();
-  const user = await userApi.getUser(appAuthCookie.id);
+  const env = envSchema.parse(process.env);
+  const response = await fetch(
+    `${env.X_URL}/api/v1/customers/${env.X_NUMBER}/points`,
+    {
+      cache: 'no-store',
+      headers: {
+        'X-Auth-API-Secret-Key': env.X_KEY,
+      },
+    },
+  );
 
-  // throw error will render nearest `error.tsx`
-  if ('message' in user) {
-    throw user.message;
+  // 403 -> gaada header 'X-Auth-API-Secret-Key' -> "Invalid Headers"
+  // 404 -> fetch failed (salah nomer) -> "Customer Not Found"
+  // 404 -> fetch failed (URL salah / VPN not connected)
+
+  if (response.status === 403) {
+    throw new Error('Invalid Headers');
+  } else if (response.status === 404) {
+    throw new Error('Customer Not Found');
   }
 
+  const points = await response.json();
+
   return (
-    <>
-      <header className="flex w-full justify-between border-b p-4 shadow-lg">
-        <GlobalNav.SidebarSheet />
-        <GlobalNav.LogoLink />
-        <GlobalNav.RoutesMenu />
-
-        <section className="flex items-center space-x-4">
-          <LoadingSingleSkeleton className="h-10 w-20 rounded-md">
-            <GlobalNav.ThemePickerPopover />
-          </LoadingSingleSkeleton>
-
-          <GlobalNav.ProfilePopover user={user} />
-        </section>
-      </header>
-
-      <main className="flex min-h-screen flex-col space-y-5 p-5">
-        <h1>Home Page</h1>
-      </main>
-    </>
+    <main className="flex min-h-screen flex-col space-y-5 p-5">
+      <pre>{JSON.stringify(points, null, 2)}</pre>
+    </main>
   );
 }
--- src/modules/shared/api/env.schema.ts
+++ src/modules/shared/api/env.schema.ts
@@ -3,4 +3,8 @@ import { z } from 'zod';
 export const envSchema = z.object({
   NEXT_PUBLIC_APP_URL: z.string().url(),
   API_BASE_URL: z.string().url(),
+
+  X_URL: z.string().url(),
+  X_NUMBER: z.string(),
+  X_KEY: z.string(),
 });
